fb13a06e1bf393b9e6d06b6977ec8d3eba91179c
Andres Rios Tascon <ariostas@gmail.com>
2025-01-24 09:41:52 -0500

    test

--- tests/test_1944_to_numpy_empty_record_array.py
+++
@@ -1,9 +0,0 @@
-# BSD 3-Clause License; see https://github.com/scikit-hep/awkward/blob/main/LICENSE
-
-from __future__ import annotations
-
-import awkward as ak
-
-
-def test():
-    assert ak.to_numpy(ak.Array([{}, {}])).tolist() == [(), ()]
